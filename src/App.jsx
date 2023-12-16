af6f18d4455558a52d6bfb6ec29b7e56711832e7
“Jonathan <jonathan.valere@gmail.com>
2023-12-16 11:46:13 -0300

    try to list categories

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,22 @@ import "./App.css";
 import movieDatas from "../src/assets/movies_netflix.json";
 
 function App() {
-  console.log(movieDatas);
+  // console.log(movieDatas);
+  const listCategories = movieDatas.map((cat, index) => {
+    // Affiche les catégories
+    return <h2 key={index}>{cat.category}</h2>;
+    // console.log(index, cat);
+    // cat.images.map((movie, index) => {
+    //   console.log(index, movie);
+    // });
+  });
+
   return (
     <>
-      <h1>Netflix App</h1>
+      <header>Netflix</header>
+      <main>
+        <section>{listCategories}</section>
+      </main>
     </>
   );
 }
